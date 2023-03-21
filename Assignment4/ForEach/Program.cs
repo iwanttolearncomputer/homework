ee8986cd9cee7fe887da2d940638272baf278d02
iwanttolearncomputer <105971524+iwanttolearncomputer@users.noreply.github.com>
2023-03-21 22:24:26 +0800

    Add files via upload

---
+++ Assignment4/ForEach/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEach
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public T Sum
+        {
+            get; set;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<Node<T>> ac)
+        {
+            Node<T> t = head;
+            while (t != null)
+            {
+                ac(t);
+                t = t.Next;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sum = 0;
+            int max = 0;
+            int min = 0x7fffffff;
+            GenericList<int> Glist = new GenericList<int>();
+            Random rand = new Random();
+            for(int n = 0; n < 10; n++)
+            {
+                Glist.Add(rand.Next(100));
+            }
+
+            //逐个输出链表元素
+            Console.Write("链表的元素为：");
+            Glist.ForEach(n => Console.Write(n.Data + "  "));
+            Console.WriteLine();
+
+            //计算链表元素之和并输出
+            Glist.ForEach(n => sum += n.Data);
+            Console.WriteLine("链表的元素的和为：" + sum);
+
+            //计算链表元素的最大值并输出
+            Glist.ForEach(n => max = max < n.Data ? n.Data : max);
+            Console.WriteLine("链表元素的最大值为：" + max);
+
+            //计算链表元素的最小值并输出
+            Glist.ForEach(n => min = min < n.Data ? min : n.Data);
+            Console.WriteLine("链表元素的最小值为：" + min);
+
+            Console.Read();
+        }
+    }
+}
---
+++ Assignment4/clock/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clock
+{
+    internal class Program
+    {
+        class Clock
+        {
+            DateTime alarmTime = DateTime.Now;
+            public delegate void AlarmControl(object sender, DateTime args);
+            public delegate void TickControl(object sender, DateTime args);
+
+            public void Tick(object sender, DateTime time)
+            {
+                Console.Write("Tick! The time now is ：");
+                Console.WriteLine(time);
+            }
+            public void Alarm(object sender, DateTime time)
+            {
+             
+                Console.Write("The alarm starts working now,the time is ：");
+                Console.WriteLine(time);
+            }
+
+            public event AlarmControl OnAlarm;
+            public event TickControl OnTick;
+            public Clock()
+            {
+                OnAlarm += Alarm;
+                OnTick += Tick;
+            }
+            public void Start()
+            {
+                Console.WriteLine("The clock starts working");
+                while (true)
+                {
+                    DateTime nowtime = DateTime.Now;
+                    OnTick(this, nowtime);
+                    if (nowtime.ToString() == alarmTime.ToString())
+                    {
+                        OnAlarm(this, alarmTime);
+                        break;
+                    }
+                    System.Threading.Thread.Sleep(1000);
+                }
+                Console.Read();
+            }
+            public void SetalarmTime(DateTime alarm)
+            {
+                Console.WriteLine("Set time to ：" + alarm);
+                alarmTime = alarm;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Clock clock = new Clock();
+
+            DateTime nowtime = new DateTime();
+            nowtime = DateTime.Now;
+            clock.Tick(clock, nowtime);
+
+            DateTime alarmtime = new DateTime();
+            //将闹钟设置为当前时间的十秒钟后
+            alarmtime = DateTime.Now.AddSeconds(10);
+            Console.WriteLine("The alarm will be ringing after 10 seconds.");
+            clock.SetalarmTime(alarmtime);
+            clock.Start();
+        }
+    }
+}
