17b64c157befaa1265eddee184c78655e1711601
iwanttolearncomputer <105971524+iwanttolearncomputer@users.noreply.github.com>
2023-04-10 22:22:22 +0800

    Add files via upload

---
+++ Assignment6/OrderManagement/Order.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement
+{
+    public class Order //一个订单
+    {
+        public int ID { get; set; }
+        public string Customer { get; set; }
+        public double Sum { get; set; }
+        public string Date { get; set; }
+        public OrderDetails orderDetail;
+        public Order() { }
+        public Order(int id, string customer, string date,OrderDetails orderdetail)
+        {
+            this.ID = id;
+            this.Customer = customer;
+            this.Date = date;
+            this.orderDetail = orderdetail;
+        }
+
+        //利用订单号判断两个订单是否相同
+        //重写equals函数，利用主码ID判断订单号是否重复
+        public override bool Equals(object obj)
+        {
+            Order order = obj as Order;
+            return this.ID == order.ID;
+        }
+        public override int GetHashCode()
+        {
+            return Convert.ToInt32(ID);
+        }
+
+        //该订单的总金额
+        public void getSum()
+        {
+            this.Sum = orderDetail.totalPrice();
+        }
+    }
+}
---
+++ Assignment6/OrderManagement/OrderDetails.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement
+{
+    public class OrderDetails               //订单明细项
+    {
+        public string Name { get; set; }
+        public int Amount { get; set; }
+        public double Price { get; set; }
+        public OrderDetails()//无参构造函数
+        {
+            this.Name = string.Empty;
+            this.Amount = 0;
+            this.Price = 0;
+        }
+        public OrderDetails(string name, int number, double price)
+        {
+            this.Name = name;
+            this.Amount = number;
+            this.Price = price;
+        }
+        public double totalPrice()
+        {
+            return this.Amount * this.Price;
+        }
+    }
+}
